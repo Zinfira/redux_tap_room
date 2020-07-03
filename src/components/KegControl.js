ca03f40ea7a3683b0559ef8359f09efdc8817951
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-02 23:15:19 -0700

    Add a condition to the KegControl component

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -5,16 +5,22 @@ import KegList from './KegList';
 class KegControl extends React.Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
        formVisibleOnPage: false
     };
   }
   
   render() {
+    let currenlyVisibleSate = null;
+    if (this.state.formVisibleOnPage) {
+      currenlyVisibleSate = <NewKegForm />
+    } else {
+      currenlyVisibleSate = <KegList />
+    }
     return (
       <React.Fragment>
-
+        {currenlyVisibleSate}
       </React.Fragment>
     );
   }
