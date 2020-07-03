49fdb3e8d0e0fe8c30228897744d47ac096b843a
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-02 22:38:20 -0700

    Add KegList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
     <React.Fragment>
-      
+      <Header />
     </React.Fragment>
   );
 }
---
+++ src/components/KegList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Keg from './Keg';
+
+function KegList() {
+  return (
+    <Keg />
+  );
+}
+
+export default KegList;
