c2639b15ec69c3d84f6278107be9404bd13e2c5f
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-02 22:47:57 -0700

    Add Keg component and PropTypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import KegList from './KegList';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <KegList />
     </React.Fragment>
   );
 }
---
+++ src/components/Keg.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props) {
+  return (
+    <React.Fragment>
+      <h3>{props.name}</h3>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+Keg.PropTypes = {
+  name: PropTypes.string.isRequired
+}
+
+export default Keg;
