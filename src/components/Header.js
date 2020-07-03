1f5db6a4c48b6e31afaf4c5caa64c24dafb759d5
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-03 12:15:44 -0700

    Add custom inline style for Header;

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 function Header() {
+  const styledHeader = {
+    display: 'flex',
+    backgroundColor: '#F8F4E3',
+    color: '#9A998C',
+    fontFamily: 'Comic Sans MS, Comic Sans, cursive',
+    justifyContent: 'center',
+  }
   return (
-   <h1>Tap Room</h1>
+    <div style={styledHeader}>
+      <h1>Tap Room</h1>
+    </div>
   );
 }
 
