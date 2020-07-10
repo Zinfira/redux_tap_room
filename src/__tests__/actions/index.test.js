3b2cdd3644e924662e973e8d714ca479bda5a558
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-10 16:19:25 -0700

    update action types in actions

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,22 +1,23 @@
 import * as actions from './../../actions';
+import * as c from './../../actions/ActionTypes';
 
 describe('tap room actions', () => {
   it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
-      type: 'DELETE_KEG',
+      type: c.DELETE_KEG,
       id: 1
     });
   });
 
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
-      type: 'TOGGLE_FORM'
+      type: c.TOGGLE_FORM
     });
   });
 
   it('addKeg should create ADD_KEG action', () => {
     expect(actions.addKeg({name: 'Kombucha', brand: 'Bucha', price: 3, flavor: 'Blueberry', pintCount: 124, id: 1})).toEqual({
-      type: 'ADD_KEG',
+      type: c.ADD_KEG,
       name: 'Kombucha',
       brand: 'Bucha',
       price: 3,
@@ -28,7 +29,7 @@ describe('tap room actions', () => {
 
   it('buyKeg should create BUY_KEG action', () => {
     expect(actions.buyKeg({name: 'Kombucha', brand: 'Bucha', price: 3, flavor: 'Blueberry', pintCount: 124, id: 1})).toEqual({
-      type: 'BUY_KEG',
+      type: c.BUY_KEG,
       name: 'Kombucha',
       brand: 'Bucha',
       price: 3,
