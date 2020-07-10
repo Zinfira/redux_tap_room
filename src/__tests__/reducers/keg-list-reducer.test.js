ca218e779d424680554b5759008bc7d62de779ae
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-10 08:17:12 -0700

    Add test to return default state if no action type is recognized and fail test

---
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -0,0 +1,7 @@
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+describe('kegListReducer', () => {
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+});
