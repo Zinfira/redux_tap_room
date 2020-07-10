76bbaa545f211448eccffc775b809e62777fd572
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-10 11:38:39 -0700

    Add code and pass test

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import formVisibleReducer from './form-visible-reducer';
+import kegListReducer from './keg-list-reducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterKegList: kegListReducer
+});
+
+export default rootReducer;
