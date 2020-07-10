d34970e45dd59cf7bc8d6be5d6597d6e8be3821c
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-07-10 08:45:27 -0700

    Add test to add new keg to masterKegList and fail test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,39 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: 'Kombucha',
+    brand: 'Brew Ms',
+    price: 4,
+    flavor: 'Blueberry',
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg to masterKegList', () => {
+    const { name, brand, price, flavor, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        flavor: flavor,
+        id: id
+      }
+    });
   });
 });
